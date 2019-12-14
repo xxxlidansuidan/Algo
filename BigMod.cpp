74183c91261308f7fc2c51aaa831d0c0a3fd1338
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-14 13:11:49 +0600

    Update BigMod.cpp

--- BigMod.cpp
+++ BigMod.cpp
@@ -1,10 +1,10 @@
 *****UVA - 374 - Big Mod (solution) :
 #include <stdio.h>
 int mod(long b, long p, long m) {
-    if(p==0)
-        return 1;
-    if(p%2==0)
-        return (mod(b,p/2,m)*mod(b,p/2,m))%m;
+    if(p==0) return 1;
+
+    if(p%2==0) return (mod(b,p/2,m)*mod(b,p/2,m))%m;
+
     return (mod(b,p-1,m)*(b%m))%m;
 }
 int main() {
